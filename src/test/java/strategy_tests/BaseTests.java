f52421c29c2da7ef31fc2bc7120c0f76b1fa968f
Oleksandr_Pochernin <Oleksandr_Pochernin@epam.com>
2017-08-01 16:19:04 +0300

    java8 stuff: code coverage for Animal class

--- src/test/java/strategy_tests/BaseTests.java
+++ src/test/java/strategy_tests/BaseTests.java
@@ -1,11 +1,11 @@
 package strategy_tests;
 
+import org.junit.Before;
+import org.junit.Test;
 import strategy.Animal;
 import strategy.Bird;
 import strategy.Dog;
 import strategy.interfaces.ItFlys;
-import org.junit.Before;
-import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,6 +19,42 @@ public class BaseTests {
     public void setUp() {
         kirk = new Dog();
         blaze = new Bird();
+    }
+
+    @Test
+    public void positiveSetHeightTest() {
+        kirk.setHeight(5);
+        assertThat(kirk.getHeight()).isEqualTo(5);
+    }
+
+    @Test
+    public void negativeValueSetToHeightTest() {
+        kirk.setHeight(-1);
+        assertThat(kirk.getHeight()).isEqualTo(1);
+    }
+
+    @Test
+    public void nullValueSetToHeightTest() {
+        kirk.setHeight(0);
+        assertThat(kirk.getHeight()).isEqualTo(1);
+    }
+
+    @Test
+    public void positiveSetWeightTest() {
+        kirk.setWeight(5);
+        assertThat(kirk.getWeight()).isEqualTo(5);
+    }
+
+    @Test
+    public void negativeValueSetToWeightTest() {
+        kirk.setWeight(-1);
+        assertThat(kirk.getWeight()).isEqualTo(1);
+    }
+
+    @Test
+    public void nullValueSetToWeightTest() {
+        kirk.setWeight(0);
+        assertThat(kirk.getWeight()).isEqualTo(1);
     }
 
     @Test

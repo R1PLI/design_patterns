cc190d8b6315a0284b9c9cc324bedfdddca8a945
R1PLI <treadz11@gmail.com>
2017-09-04 17:30:19 +0300

    java8 stuff: usage of 'optional' values

--- src/main/java/java8/venkat/Filters.java
+++ src/main/java/java8/venkat/Filters.java
@@ -1,9 +1,30 @@
 package java8.venkat;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static java.util.Arrays.asList;
+
 public class Filters {
+
+	public static void main(String[] args) {
+		List<String> friends = new ArrayList<>(asList("Bla", "Bla"));
+
+		final Optional<String> aLongName =
+			friends.stream()
+				.reduce((name1, name2) ->
+					name1.length() >= name2.length() ? name1 : name2);
+
+		aLongName.ifPresent(name ->
+			System.out.println(String.format("A longest name: %s", name)));
+
+
+		final String steveOrLonger =
+			friends.stream()
+				.reduce("Steve", (name1, name2) ->
+					name1.length() >= name2.length() ? name1 : name2);
+	}
 
 	public static void pickNameOld(
 		final List<String> names, final String startingLetter) {
@@ -15,7 +36,7 @@ public class Filters {
 			}
 		}
 		System.out.print(String.format("A name starting with %s: ", startingLetter));
-		if(foundName != null) {
+		if (foundName != null) {
 			System.out.println(foundName);
 		} else {
 			System.out.println("No name found");
@@ -26,7 +47,7 @@ public class Filters {
 		final List<String> names, final String startingLetter) {
 		final Optional<String> foundName =
 			names.stream()
-				.filter(name ->name.startsWith(startingLetter))
+				.filter(name -> name.startsWith(startingLetter))
 				.findFirst();
 
 		System.out.println(String.format("A name starting with %s: %s",

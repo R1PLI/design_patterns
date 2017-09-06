ce269c6fc8d606cf6073b3f198f343e3a5a231fc
R1PLI <treadz11@gmail.com>
2017-09-06 17:36:33 +0300

    java 8 stuff: remove redundant variable

--- src/main/java/java8/venkat/StringsIteration.java
+++ src/main/java/java8/venkat/StringsIteration.java
@@ -5,11 +5,6 @@ import java.util.List;
 import static java.util.stream.Collectors.toList;
 
 public class StringsIteration {
-	public static void main(String[] args) {
-		String str = "w00t";
-
-	}
-
 	public List<Character> splitStringIntoChars(String str) {
 		return str.chars()
 			.mapToObj(ch -> (char) ch)

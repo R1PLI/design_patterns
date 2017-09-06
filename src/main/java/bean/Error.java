cbd026817cf88de10d206060357f2d9617cfb8b1
R1PLI <treadz11@gmail.com>
2017-09-06 17:29:36 +0300

    master: clean redundant files from master

--- src/main/java/bean/Error.java
+++
@@ -1,9 +0,0 @@
-package bean;
-
-import lombok.Data;
-
-@Data
-public class Error {
-	private String status;
-	private String message;
-}
--- src/main/java/helper/json/JsonMapper.java
+++
@@ -1,4 +0,0 @@
-package helper.json;
-
-public class JsonMapper {
-}
